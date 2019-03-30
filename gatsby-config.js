de4fbe2359e220a10fe52a2664bfd7dcaef1bbfc
Bryan McConnahea <transcendrstudio@gmail.com>
2019-03-30 12:48:35 +0100

    Sets up contentful source plugin for use

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,37 @@
+let contentfulConfig
+
+try {
+  // Load the Contentful config from the .contentful.json
+  contentfulConfig = require("./.contentful")
+} catch (_) {}
+
+// Overwrite the Contentful config with environment variables if they exist
+contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
+  accessToken:
+    process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken
+}
+
+const { spaceId, accessToken } = contentfulConfig
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    "Contentful spaceId and the delivery token need to be provided."
+  )
+}
+
 module.exports = {
   siteMetadata: {
     name: `Hello Typescript World!`,
     tagline: `Gatsby + SASS + Typescript = 💪`
-  },  
+  },
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-tslint`,
-  ],
+    {
+      resolve: "gatsby-source-contentful",
+      options: contentfulConfig
+    }
+  ]
 }
