33caeb670d7073c18da6fda195e35b7b00a80e71
Bryan McConnahea <transcendrstudio@gmail.com>
2019-04-02 23:07:13 +0200

    Netlify deploy post-install test

--- _deploy/post-install.sh
+++ _deploy/post-install.sh
@@ -2,4 +2,8 @@
 
 echo "-------------------- RUNNING POST INSTALL ------------------------"
 pwd
+mkdir public && mkdir public/assets
+ls public
+cp -a ./src/assets ./public
+ls public
 echo "-------------------- RUNNING POST INSTALL ------------------------"
